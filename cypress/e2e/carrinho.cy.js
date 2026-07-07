9a5562196e9a14ae404c168debfc1607c362cbad
Pablo Soares Lopes <mpablosl@gmail.com>
2026-07-07 18:15:44 -0300

    casso de teste adicionado TC-CARRINHO-003.md  e nova criação de teste automatizado em carrinho.cy.js

---
+++ cypress/e2e/carrinho.cy.js
@@ -0,0 +1,47 @@
+describe ('Suíte de Testes de Carrinho - Saucedemo', () =>{
+
+    // PRÉ-CONDIÇÃO: O usuário deve estar autentiacdo no sistema para realizar os testes do carrinho. Para isso, vamos utilizar o comando customizado "fazerLogin" que está localizado no arquivo cypress/support/commands.js
+    beforeEach(() => {
+        cy.visit('https://www.saucedemo.com/')
+        cy.fazerLogin('standard_user','secret_sauce')
+    })
+
+    it('TC-CARRINHO-001 - Adicionar um produto ao carrinho com sucesso', ()=>{
+        // 1 - Clicar no botão "Add to cart" do produto Sauce Labs Bike Light"
+        cy.get('[data-test="add-to-cart-sauce-labs-bike-light"]').click()
+
+        // 2 - RESULTADO ESPERADO 1: Validar se o ícone do carrinho de compras está visível na tela, indicando que o produto foi adicionado com sucesso
+        // ACHAR (cy.get) e VERIFICAR (.should) 
+        cy.get('[data-test="shopping-cart-badge"]').should('have.text', '1')
+
+        // 3 - RESULTADO ESPERADO 2: O botão deve mudar de "Add to cart" para "Remove", indicando que o produto foi adicionado com sucesso.
+        //ACHAR (cy.get) e VERIFICAR (.should)
+        cy.get('[data-test="remove-sauce-labs-bike-light"]').should('have.text', 'Remove')
+
+    })
+
+    it('TC-CARRINHO-004 - Adicionar múltiplos produtos ao carrinho com suceeso', () =>{
+
+        // 1 - Clicar no botão "Add to cart" do produto "Sauce Labs Bike Light"
+        cy.get('[data-test="add-to-cart-sauce-labs-bike-light"]').click()
+        // 1.1 - Resultado Esperado: Verifica se o ícone do carrinho de compras está visível na tela, indicando que o produto foi adicionado com suceso.
+        cy.get('[data-test="shopping-cart-badge"]').should('have.text', '1')
+        // 1.2 - Resultado Esperado: Verifica se o botão "Add to cart" do  produto "Sauce Labs Bike Light" mudou para "Remove", indicando que o produto foi adicionado com sucesso.
+        cy.get('[data-test="remove-sauce-labs-bike-light"]').should('have.text', 'Remove')
+
+        // 2 - Clicar no botão "Add to cart" do produto "Sauce Labs Bolt T-Shirt"
+        cy.get('[data-test="add-to-cart-sauce-labs-bolt-t-shirt"]').click()
+        // 2.1 - Resultado Esperado: Verifica se o ícone do carrinho de compras está visível na tela, indicando que o produto foi adicionado com suceso.
+        cy.get('[data-test="shopping-cart-badge"]').should('have.text', '2')
+        // 2.2 - Resultado Esperado: Verifica se o botão "Add to cart" do  produto "Sauce Labs Bolt T-Shirt" mudou para "Remove", indicando que o produto foi adicionado com sucesso.
+        cy.get('[data-test="remove-sauce-labs-bolt-t-shirt"]').should('have.text', 'Remove')
+
+        // 3 - Clicar no botão "Add to cart" do produto "Sauce Labs Onesie"
+        cy.get('[data-test="add-to-cart-sauce-labs-onesie"]').click()
+        // 3.1 - Resultado Esperado: Verifica se o ícone do carrinho de compras está visível na tela, indicando que o produto foi adicionado com suceso.
+        cy.get('[data-test="shopping-cart-badge"]').should('have.text', '3')
+        // 3.2 - Resultado Esperado: Verifica se o botão "Add to cart" do  produto "Sauce Labs Onesie" mudou para "Remove", indicando que o produto foi adicionado com sucesso.
+        cy.get('[data-test="remove-sauce-labs-onesie"]').should('have.text', 'Remove')       
+        
+    })
+})
--- docs/test-cases/TC-CARRINHO-003.md
+++ docs/test-cases/TC-CARRINHO-003.md
@@ -1,18 +1,18 @@
 ﻿
-### TC-CARRINHO-001 — Remover um produto do carrinho com sucesso
+### TC-CARRINHO-001 — Remover um produto pela página do carrinho com sucesso
 
 **ID**
 TC-CARRINHO-002
 
 **Título**
-Remover um produto do carrinho com sucesso
+Remover um produto pela página do carrinho com sucesso
 
 **Prioridade**
 Alta
 
 **Pré-condição** 
 - Usuário deve estar autenticado no sistema
-- Usuário deve estar na página de produtos
+- Usuário deve estar na página de carrinho
 - Ter produto "Sauce Labs Bike Light" adicionado ao carrinho
 
 **Dados de Teste**
@@ -21,12 +21,14 @@ Password: secret_sauce
 Produto: Sauce Labs Bike Light
 
 **Passos**
-1 - Visualizar a lista de produtos na página de inventário -> https://www.saucedemo.com/inventory.html
+1 - Visualizar a lista de produtos na página de carrinho "Your Cart" -> https://www.saucedemo.com/cart.html
 2 - Clicar no botão "Remove" do produto "Sauce Labs Bike Light"
-3 - Observar o ícone do carrinho de compras no canto superior direito da tela
+3 - Observar se o produto desaparece da página
+4 - Observar o ícone do carrinho de compras no canto superior direito da tela
 
 **Resultado Esperado**
-1 - O botão do produto "Sauce Labs Bike Light" deve mudar o seu texto de "Add to cart" para "Remove"
-2 - O ícone vermelho do carrinho de compras deve desaparecer (ou deixar de exibir números)
+1 - A linha contendo o produto "Sauce Labs Bike Light" deve desaparecer (ser removida) da página
+2 - O ícone vermelho do carrinho de compras deve desaparecer (pois não há mais itens).
+2 - O usuário deve permanecer na página "Your Cart"
 
 ----------
